72bf02d5762b12ccc3eff6fcf422da20fb9ce996
Joost van der Griendt <joostvdg@gmail.com>
2019-11-22 11:22:37 +0100

    update example to leverage excludes
    Signed-off-by: Joost van der Griendt <joostvdg@gmail.com>

--- pipeline-examples/matrix-build-podtemplate.groovy
+++ pipeline-examples/matrix-build-podtemplate.groovy
@@ -14,56 +14,59 @@ pipeline {
                         values 'ibmjava','amazoncorretto', 'jdk'
                     }
                 }
+                excludes {
+                    exclude {
+                        axis {
+                            name 'JDK_VERSION'
+                            values '13'
+                        }
+                        axis {
+                            name 'JDK_TYPE'
+                            notValues 'jdk' // double negative, make sure we only do 13 with jdk
+                        }
+                    }
+                    exclude {
+                        axis {
+                            name 'JDK_VERSION'
+                            values '11'
+                        }
+                        axis {
+                            name 'JDK_TYPE'
+                            values "ibmjava"
+                        }
+                    }
+                }
+                agent {
+                    kubernetes {
+                        label "maven-${JDK_TYPE}-${JDK_VERSION}-test"
+                        containerTemplate {
+                            name 'maven'
+                            image "maven:3-${JDK_TYPE}-${JDK_VERSION}"
+                            ttyEnabled true
+                            command 'cat'
+                        }
+                    }
+                }
                 stages {
-                    stage("Get Agent") {
-                        when {
-                            allOf {
-                                not {
-                                    allOf {
-                                        environment name: 'JDK_VERSION' , value: '13'
-                                        environment name: 'JDK_TYPE'    , value: 'amazoncorretto'
-                                    } 
-                                }
-                                not {
-                                    allOf {
-                                        environment name: 'JDK_VERSION' , value: '11'
-                                        environment name: 'JDK_TYPE'    , value: 'ibmjava'
-                                    } 
-                                }
-                                not {
-                                    allOf {
-                                        environment name: 'JDK_VERSION' , value: '13'
-                                        environment name: 'JDK_TYPE'    , value: 'ibmjava'
-                                    } 
-                                }
-                            }
-                            beforeAgent true
-                        }
-                        agent {
-                            kubernetes {
-                                label "maven-${JDK_TYPE}-${JDK_VERSION}-test"
-                                containerTemplate {
-                                    name 'maven'
-                                    image "maven:3-${JDK_TYPE}-${JDK_VERSION}"
-                                    ttyEnabled true
-                                    command 'cat'
-                                }
-                            }
-                        }
+                    stage("Build Matrix") {
                         stages {
+                            stage('Test Image') {
+                                steps {
+                                    println "Using Image: maven:3-${JDK_TYPE}-${JDK_VERSION}"
+                                }
+                            }
                             stage("Build") {
                                 steps {
                                     sh 'uname -a'
                                     git 'https://github.com/joostvdg/jx-maven-lib.git'
                                     container('maven') {
-                                        sh 'mvn -version'
-                                        sh 'mvn clean verify'
+                                        sh 'mvn clean verify --show-version --strict-checksums -e'
                                     }
                                 }
                             }
-                            stage("Compat") {
+                            stage("Deploy") {
                                 when {
-                                    environment name: "JDK_VERSION", value: "7"
+                                    branch 'master'
                                 }
                                 steps {
                                     echo "WE SHOULD NEVER GET HERE"
